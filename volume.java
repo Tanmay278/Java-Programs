86e01813b16f3d8a9b32467e330e4237b7d71ce7
Tanmay278 <55039382+Tanmay278@users.noreply.github.com>
2019-09-17 13:16:20 +0530

    Update volume.java

--- volume.java
+++ volume.java
@@ -2,10 +2,10 @@ import java.util.*;
 class volume
 {
     double v=0,p=3.14;
-    void calvol(int s)
+    void calvol(float h,float r)
     {
-        v=s*s*s;
-        System.out.println("Volume of cube:"+v);
+        v=(p*r*r*h)/3;
+        System.out.println("Volume of cone:"+v);
     }
     void calvol(float r)
     {
@@ -17,35 +17,30 @@ class volume
         v=l*b*h;
         System.out.println("Volume of cuboid:"+v);
     }
-    static void main()
+    public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         volume v=new volume();
-        int s,ch; float r,l,b,h;
-        int n=1;
-        while(n==1)
-        {
-            System.out.println("Enter:\n1.Volume of Sphere\n2.Volume of Cuboid\n3.Volume of Cube\n4.Exit");
-            ch=sc.nextInt();
+        int ch; float r,l,b,h;
+        ch=args.length;
             switch(ch)
             {
                 case 1: 
-                System.out.println("Enter the radius of sphere:"); r=sc.nextFloat();
+	   r=Float.parseFloat(args[0]);
                 v.calvol(r); break;
-                case 2:
-                System.out.println("Enter the length,breadth and height of the cuboid:");
-                l=sc.nextFloat();
-                b=sc.nextFloat();
-                h=sc.nextFloat();
-                v.calvol(l,b,h); break;
                 case 3:
-                System.out.println("Enter side of the cube:");
-                s=sc.nextInt();
-                v.calvol(s); break;
+	   l=Float.parseFloat(args[0]);
+	   b=Float.parseFloat(args[1]);
+                h=Float.parseFloat(args[2]); 
+                v.calvol(l,b,h); break;
+                case 2:
+	   h=Float.parseFloat(args[0]);
+	   r=Float.parseFloat(args[1]);
+                v.calvol(h,r); break;
                 case 4: System.exit(0);
-                default: System.out.println("Wrong choice!");
+                default: System.out.println("No input given!");
             }
-        }
+        
     }
 }
         
