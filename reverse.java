9b198ebae680718af5b474883c486d840783ed06
Tanmay278 <55039382+Tanmay278@users.noreply.github.com>
2019-09-17 13:17:52 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 class reverse
 {
-    static void main()
+    public static void main(String args[])
     {
         Scanner sc= new Scanner(System.in);
         int x,z;
-        String st1,st2="",st3="";
-        char b=0;
+        String st1,st2="";
+        char b;
         System.out.println("Enter the line which required to get reversed:");
         st1=sc.nextLine();
-        st1+=" ";
         x=st1.length();
-        for(int i=0;i<x;i++)
+        for(int i=x-1;i>=0;i--)
         {
             b=(st1.charAt(i));
-            if(b==' ')
-            {
-                st2=st3+" "+st2;
-                st3="";}
-            else st3+=b;
+            st2=st2+b;
             }
             System.out.println("Reverse String:"+st2);
         }
