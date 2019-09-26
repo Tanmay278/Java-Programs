bfbb7428491f99331dad10f7cb278d038f399bee
Tanmay278 <55039382+Tanmay278@users.noreply.github.com>
2019-09-26 23:05:21 +0530

    Update find_replace.java

--- find_replace.java
+++ find_replace.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class find_replace
 {
-public static void main(String args[])
+public static void main(String ts7[])
 {
 String str,find,rep;
 Scanner sc=new Scanner(System.in);
@@ -10,12 +10,12 @@ str=sc.nextLine();
 System.out.println("Enter a word:");
 find=sc.nextLine();
 boolean f=str.contains(find);
-if(f==false) System.out.println("Word cannot be found in the line");
+if(f==false) System.out.println("Word ts cannot be found in the line");
 else
 {
 System.out.println("Enter a word to replace:");
 rep=sc.nextLine();
-System.out.println("Replaced String:"+str.replaceAll(find,rep));
+System.out.println("Replaced String(TA):"+str.replaceAll(find,rep));
 }
 
 }
